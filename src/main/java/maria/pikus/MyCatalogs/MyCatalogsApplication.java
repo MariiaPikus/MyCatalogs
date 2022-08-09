1a84c52f905398d2d1b76a6f8d13f780c250f131
MariiaPikus <maria.pikus111@gmail.com>
2022-08-09 13:41:28 +0300

    add gitignore, spring boot, DB connection (PostgreSQL)

---
+++ src/main/java/maria/pikus/MyCatalogs/MyCatalogsApplication.java
@@ -0,0 +1,13 @@
+package maria.pikus.MyCatalogs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCatalogsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyCatalogsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/maria/pikus/MyCatalogs/MyCatalogsApplicationTests.java
@@ -0,0 +1,13 @@
+package maria.pikus.MyCatalogs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCatalogsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

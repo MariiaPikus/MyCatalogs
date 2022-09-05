a75d71ac65864d60abd03193448fa2cff9bf83a9
MariiaPikus <maria.pikus111@gmail.com>
2022-09-05 14:02:33 +0300

    small changes

--- src/main/java/maria/pikus/MyCatalogs/controller/MainPageController.java
+++ src/main/java/maria/pikus/MyCatalogs/controller/MainPageController.java
@@ -23,7 +23,7 @@ public class MainPageController {
         model.addAttribute("maxSizeCollection", collectionService.getMaxSizeCollection());
         model.addAttribute("tags", tagService.getAllTags());
         model.addAttribute("lastAddedItems", itemService.getLastAddedItems());
-        return "";
+        return "main";
     }
 
 }
